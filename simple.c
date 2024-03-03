b146ac39c269d0e2b0d1bf2e970c74fd403b98a8
Vinayak <hosamanivinayak5@gmail.com>
2024-03-03 16:12:06 +0530

    Edited simple.c

--- simple.c
+++ simple.c
@@ -1,34 +1,51 @@
 #include<stdio.h>
 
-int main()
-{
+int main() {
     char ch;
-int a,b,c;
-while (1)
-{    printf("Enter operand a:");
-scanf("%d",&a);
-printf("Enter operand b:");
-scanf("%d",&b);
-    printf("Enter operator:");
-scanf("%*c",&ch);
+    int a, b, c;
 
-    switch (ch)
-    {
-    case '+':
-        c=a+b;
-        break;
-    case '-':  c=a-b;
-    break;
-    case'*' :  c=a*b;
-    break;
-case  '/':  c=a/b;
-break;
+    while (1) {
+        printf("Enter operand a: ");
+        scanf("%d", &a);
 
-    default:
-        break;
+        printf("Enter operand b: ");
+        scanf("%d", &b);
+
+        printf("Enter operator: ");
+        scanf(" %c", &ch); // Added space before %c to consume any leading whitespace
+
+        switch (ch) {
+            case '+':
+                c = a + b;
+                break;
+            case '-':
+                c = a - b;
+                break;
+            case '*':
+                c = a * b;
+                break;
+            case '/':
+                if (b != 0) {
+                    c = a / b;
+                } else {
+                    printf("Error: Division by zero\n");
+                    continue; 
+                }
+                break;
+            default:
+                printf("Error: Invalid operator\n");
+                continue; 
+        }
+
+        printf("Answer: %d\n", c);
+        
+     
+        printf("Do you want to continue (Y/N)? ");
+        scanf(" %c", &ch); 
+        if (ch == 'N' || ch == 'n') {
+            break;
+        }
     }
-    printf("answer %d\n",c);
-}
 
-
+    return 0;
 }
